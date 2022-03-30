089614e18a4342d926c1b6248ef6ac07e1136542
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-30 16:33:57 +0800

    Almost completed the home layout

---
+++ components/AppFooter.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <div id="wrapper">
+        Some disclaimer crap in here
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/AppHeader.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/AppHeader.module.scss'
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <header>
+      <div id="wrapper">
+        <div id={styles.headerDesktop}>
+          <strong id={styles.logo}>
+            <Link href="/">Searchpets</Link></strong>
+          <nav id={styles.headerNav}>
+            <Link href="/api">For developers</Link>
+            <Link href="/about">About</Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
