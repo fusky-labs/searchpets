4393e85aa9d904400e587a2d83f660cb92798fb3
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-07 20:49:12 +0800

    Remove `src` from ComicItem component

--- app/components/ComicItem.tsx
+++ app/components/ComicItem.tsx
@@ -1,21 +1,18 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 interface IComicItemProps {
   title?: string;
   characters: string;
   link: string;
-  src?: string;
 }
 
 export default function ComicItem({
-  title, characters, link, src
+  title, characters, link
 }: IComicItemProps) {
   return (
     <div id="comic-item" className="drop-shadow-md rounded-md p-4">
       <h3>{title}</h3>
       <Link href={link} passHref>
-        {/* <Image src={src} alt={title} /> */}
         <span>{characters}</span>
       </Link>
     </div>
