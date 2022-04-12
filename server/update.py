2696f0d2c9cd4f7d1fd4fd854a5b9f91ab0c656d
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-04-12 21:16:28 +0800

    shortening long variable names
    changing varible case because wow
    
    renaming doodoo
    
    just a script to manually update the db
    
    [...]
    
    typo

---
+++ server/update.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+
+current_date_time = datetime.datetime.now()
+date = current_date_time.date()
+year = date.strftime("%Y")
+
+# open json file
+with open('housepets_db.json', 'r') as housepets_db_json:
+    housepets_db = json.load(housepets_db_json)
+
+housepets_db.update({year: []})
+print(year)
+web = requests.get(f"https://www.housepetscomic.com/archive/?archive_year={year}")
+soup = BeautifulSoup(web.text, 'html.parser')
+link_tag = soup.find_all('a', {'rel':"bookmark", 'href': re.compile("^https://")})
+print(len(link_tag))
+for link in link_tag:
+    web_link = link.get('href')
+    web_link_page = requests.get(web_link)
+    if "https://www.housepetscomic.com/character" in web_link_page.text:
+        print(f'{web_link} is a real comic by rick grifin')
+
+        # get the characters from the comic
+        characters_in_comic = []
+        characters_in_comic_soup = BeautifulSoup(web_link_page.text, 'html.parser')
+        characters_in_comic_tag = characters_in_comic_soup.find_all('a', {'href': re.compile("^https://www.housepetscomic.com/character")})
+        for character in characters_in_comic_tag:
+            characters_in_comic.append(character.text)
+        housepets_db[year].append({'comic_link': web_link, 'characters': characters_in_comic})
+
+print(housepets_db)
+with open('housepets_db.json', 'w') as housepets_db_json:
+    json.dump(housepets_db, housepets_db_json)
