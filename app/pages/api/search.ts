3a33a97f48298c9a8175d80fde1b379d220f87c3
thatITfox <maxthecomputerfox@gmail.com>
2022-04-15 07:29:48 +0700

    this will also use async await

--- app/pages/api/search.ts
+++ app/pages/api/search.ts
@@ -8,16 +8,17 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // send the data to the flask server
-  fetch('http://localhost:5000/search', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(req.body)
-  })
-    .then(response => response.json())
-    .then(data => {
-      res.status(200).json(data)
+  // send the data to the flask server using fetch and async/awaits
+  const sendData = async () => {
+    const response = await fetch('http://localhost:5000/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
     })
+    const json = await response.json()
+    res.status(200).json(json)
+  }
+  sendData()
 }
