2a25564f9e64d2e9409467e8eb2744a7c1698e7e
thatITfox <maxthecomputerfox@gmail.com>
2022-04-30 13:30:25 +0700

    added a init for windows users

---
+++ win-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install concurrently lib
+yarn install
+
+# Install the Next app
+yarn --cwd ./app install
+
+# Install python backend
+cd server
+pip install -r requirements.txt
+
+# Generate database
+python gen.py
+
+# Install Sharp for image loading for production, build, and start the server
+yarn add sharp
+yarn build
+yarn start
