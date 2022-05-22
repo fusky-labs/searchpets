74aa10ceb39ce87d2da8c486e49daf7f7152a981
thatITfox <maxthecomputerfox@gmail.com>
2022-05-22 17:12:58 +0700

    change it to localhost so it won't scream about addres already used

--- server/app.py
+++ server/app.py
@@ -85,4 +85,4 @@ if __name__ == '__main__':
     from waitress import serve
     print("[*]starting server")
     print("[*]server is running on port 5000")
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='localhost', port=5000)
