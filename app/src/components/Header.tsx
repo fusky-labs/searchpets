6a69d4c95732181bf707d06a296ab5b30240e69f
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-05-28 13:47:04 +0800

    More about the project

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
-
 import { useRouter } from "next/router"
+import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome"
+import { faInfoCircle, faList, faSearch } from "@fortawesome/free-solid-svg-icons"
 
 export default function Header() {
   const router = useRouter()
@@ -8,19 +9,22 @@ export default function Header() {
   return (
     <header>
       <div className="content-wrapper flex justify-center text-4xl">
-        <nav className="flex text-xl gap-x-4 flex-col md:flex-row">
+        <nav className="flex text-xl gap-x-7 flex-col md:flex-row">
           <Link href="/" passHref>
             <a className={router.pathname == "/" ? "text-blue-400 font-bold" : ""}>
-              Searchpets
+              <FaIcon icon={faSearch} size="sm" className="mr-2" />
+              Search
             </a>
           </Link>
           <Link href="/characters" passHref>
             <a className={router.pathname == "/characters"? "text-blue-400 font-bold": ""}>
+              <FaIcon icon={faList} size="sm" className="mr-2" />
               Character List
             </a>
           </Link>
           <Link href="/about" passHref>
             <a className={router.pathname == "/about" ? "text-blue-400 font-bold" : ""}>
+              <FaIcon icon={faInfoCircle} size="sm" className="mr-2" />
               About this project
             </a>
           </Link>
