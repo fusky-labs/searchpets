8fe7d321a897dbfafb578d08a49c45a0746e398e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-05 15:21:09 +0800

    Added Google Analytics

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -4,10 +4,24 @@ import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import "../styles/globals.scss"
 import "react-notifications-component/dist/theme.css"
+import * as ga from "../lib/ga"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 config.autoAddCss = false
 
 export default function SearchpetsApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  const handleRouteChange = (url) => {
+    ga.pageview(url)
+  }
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => router.events.off("routeChangeComplete", handleRouteChange)
+  }, [router.events])
+
   return (
     <Layout>
       <Component {...pageProps} />
