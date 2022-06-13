5b348b9e3cdf8681699acd725ca114aafba05166
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-14 01:33:54 +0800

    Added google analytics

---
+++ app/lib/ga.ts
@@ -0,0 +1,15 @@
+declare global {
+  interface Window {
+    gtag: any
+  }
+}
+
+export const pageview = (url) => {
+  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+    page_path: url,
+  })
+}
+
+export const event = ({ action, params }) => {
+  window.gtag("event", action, params)
+}
