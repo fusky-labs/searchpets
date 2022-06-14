4298bad5ca66e6a8739c300ebde645bcca4c0538
thatITfox <78403482+thatITfox@users.noreply.github.com>
2022-06-14 10:56:56 +0700

    Fast api change (#26)
    * Added fastAPI to the requirements and imports
    
    * Implemented fastAPI for our api's, ready for testing
    
    * Shorten the variable names for /data
    
    * Use `async def`
    
    * Shortening variable names
    
    * Added google analytics (#25)
    
    * Added `argpase` on server
    
    * Minor setup.sh tweaks
    
    * OMG A TYPO
    
    * Formatting
    
    * moving `.env.local.example`
    
    * Removing babel
    
    Co-authored-by: Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
    Co-authored-by: skepfusky <skepfoosky15@gmail.com>

--- server/app.py
+++ server/app.py
@@ -1,24 +1,38 @@
-from traceback import print_tb
-from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+from fastapi import FastAPI
+from pydantic import BaseModel
+import argparse
 import json
-import os
 import threading
 import time
 import requests as req
-from bs4 import BeautifulSoup
 import re
+import uvicorn
+
+parser = argparse.ArgumentParser(description="Searchpets FastAPI Server")
+parser.add_argument('-b', '--build', action="store_true",
+                    help="Run the server in production mode, for development, don't provide with any arguments and run the file as is.")
+args = parser.parse_args()
+
 
 with open('housepets_db.json', 'r') as housepets_db_json:
     housepets_db = json.load(housepets_db_json)
+
+"""
+Update database
+"""
+
 
 def update_database():
     while True:
         time.sleep(600)
         characters_db = set(housepets_db['characters_db'])
         year = time.strftime("%Y")
-        web = req.get(f"https://www.housepetscomic.com/archive/?archive_year={year}")
+        web = req.get(
+            f"https://www.housepetscomic.com/archive/?archive_year={year}")
         soup = BeautifulSoup(web.text, 'html.parser')
-        link_tag = soup.find_all('a', {'rel':"bookmark", 'href': re.compile("^https://")})
+        link_tag = soup.find_all(
+            'a', {'rel': "bookmark", 'href': re.compile("^https://")})
 
         comics_db = []
 
@@ -30,20 +44,24 @@ def update_database():
                 if "https://www.housepetscomic.com/character" in web_link_page.text:
                     print(f'{web_link} is a real comic by rick grifin')
 
-                    characters_in_comic = []
-                    characters_in_comic_soup = BeautifulSoup(web_link_page.text, 'html.parser')
-                    characters_in_comic_tag = characters_in_comic_soup.find_all('a', {'href': re.compile("^https://www\.housepetscomic\.com/character")})
-                    for character in characters_in_comic_tag:
-                        characters_in_comic.append(character.text.lower())
+                    chars = []
+                    chars_soup = BeautifulSoup(
+                        web_link_page.text, 'html.parser')
+                    chars_tag = chars_soup.find_all(
+                        'a', {'href': re.compile("^https://www\.housepetscomic\.com/character")})
+                    for character in chars_tag:
+                        chars.append(character.text.lower())
                     # the code under this will find the image where there is a tittle and alt
-                    comic_image = characters_in_comic_soup.find('img', {'title': True, 'alt': True})
-                    comics_db.append({"title": characters_in_comic_soup.title.text.split(' \u2013 ')[0],
-                                            "comic_link": web_link,
-                                            "characters": characters_in_comic,
-                                            "image": comic_image.get('src')})
-                    characters_db.update(characters_in_comic)
-            housepets_db['characters_db'] = list(characters_db) # update the characters db
-            housepets_db[year] = comics_db # update the comics db
+                    comic_image = chars_soup.find(
+                        'img', {'title': True, 'alt': True})
+                    comics_db.append({"title": chars_soup.title.text.split(' \u2013 ')[0],
+                                      "comic_link": web_link,
+                                      "characters": chars,
+                                      "image": comic_image.get('src')})
+                    characters_db.update(chars)
+            housepets_db['characters_db'] = list(
+                characters_db)  # update the characters db
+            housepets_db[year] = comics_db  # update the comics db
             with open('housepets_db.json', 'w') as housepets_db_json:
                 json.dump(housepets_db, housepets_db_json)
             print("[*] Database updated!")
@@ -51,40 +69,52 @@ def update_database():
         else:
             print("[*] Database is currently up to date!")
 
+
 threading.Thread(target=update_database).start()
 
-app = Flask(__name__)
 
-@app.route('/search', methods=['POST'])
-def test():
-    print(request.json)
-    years = request.json['year']
-    characters = request.json['characters']
+class Search(BaseModel):
+    characters: list = []
+    year: list = []
+
+
+app = FastAPI()
+
+
+@app.post('/search')
+async def search(search: Search):
+    characters = search.characters
+    years = search.year
+    print(characters)
+    print(years)
     comics = []
     for year in years:
-        # check if the year is a key in the database
-        if year in housepets_db:
-            print(f'[*] {year} is a valid year')
-            for comic in housepets_db[year]:
-                if all(character in comic['characters'] for character in characters):
-                    comics.append(comic)
-    print(f"[*] {len(comics)} comics found")
-    return jsonify({'comics': comics})
+        for comic in housepets_db[year]:
+            if all(character in comic['characters'] for character in characters):
+                comics.append(comic)
+    return {'comics': comics}
 
-@app.route('/data', methods=['GET'])
-def data():
-    housepets_db_length = 0
+
+@app.get('/data')
+async def data():
+    comic_length = 0
     for year in range(2008, 2022+1):
-        housepets_db_length += len(housepets_db[str(year)])
-    characters_db_length = len(housepets_db['characters_db'])
-    return jsonify({'comicCount': housepets_db_length, 'charCount': characters_db_length})
+        comic_length += len(housepets_db[str(year)])
+    chars_length = len(housepets_db['characters_db'])
+    return {'comicCount': comic_length, 'charCount': chars_length}
 
-@app.route('/characters', methods=['GET'])
-def characters():
-    return jsonify({'characters_db': housepets_db['characters_db']})
+
+@app.get('/characters')
+async def characters():
+    return {'characters_db': housepets_db['characters_db']}
 
 if __name__ == '__main__':
-    from waitress import serve
-    print("[*] Starting server...")
-    print("[*] Server running on port 5000.")
-    serve(app, host='localhost', port=5000)
+    if args.build:
+        print("[*] Server started in build/production mode")
+        print("[*] Server running on port 5000.")
+        uvicorn.run("app:app", host="0.0.0.0", port=5000)
+
+    else:
+        print("[*] Server started in development mode")
+        uvicorn.run("app:app", host="0.0.0.0",
+                    port=5000, reload=True, debug=True)
--- server/gen.py
+++ server/gen.py
@@ -14,7 +14,7 @@ user_agent = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)'
                              'Chrome/45.0.2454.101 Safari/537.36'),
                              'referer': 'https://www.housepetscomic.com'}
 
-years = ['2008', "2009", "2010", "2011", "2012", "2013", "2014", 
+years = ['2008', "2009", "2010", "2011", "2012", "2013", "2014",
         "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]
 
 housepets_db = {}
@@ -24,36 +24,36 @@ characters_db = set()
 for year in years:
     housepets_db.update({year: []})
     print(f"Searching in year {Fore.GREEN}{Style.BRIGHT}{year}{Style.RESET_ALL}")
-    
+
     web = requests.get(f"https://www.housepetscomic.com/archive/?archive_year={year}", headers=user_agent, timeout=None)
     soup = BeautifulSoup(web.text, 'html.parser')
     link_tag = soup.find_all('a', {'rel':"bookmark", 'href': re.compile("^https://")})
     print(f"Found {Fore.GREEN}{Style.BRIGHT}{len(link_tag)}{Style.RESET_ALL} tags!")
-    
+
     for link in link_tag:
-        web_link = link.get('href')
-        web_link_page = requests.get(web_link, headers=user_agent, timeout=None)
-        if "https://www.housepetscomic.com/character" in web_link_page.text:
-            print(web_link)
+        link = link.get('href')
+        link_page = requests.get(link, headers=user_agent, timeout=None)
+        if "https://www.housepetscomic.com/character" in link_page.text:
+            print(link)
 
             characters = []
-            comic_soup = BeautifulSoup(web_link_page.text, 'html.parser')
+            comic_soup = BeautifulSoup(link_page.text, 'html.parser')
             characters_tag = comic_soup.find_all('a', {'href': re.compile("^https://www\.housepetscomic\.com/character")})
             for character in characters_tag:
                 characters.append(character.text.lower())
                 characters_db.add(character.text.lower())
-            
+
             comic_image = comic_soup.find('img', {'title': True, 'alt': True})
 
             print(comic_image.get('src'))
             housepets_db[year].append({
                 'title':comic_soup.title.text.split(' \u2013 ')[0], # The character "u\u2013" is the unicode for the dash
-                'comic_link': web_link, 
+                'comic_link': link,
                 'characters': characters,
                 'image': comic_image.get('src')
-                }) 
+                })
         else:
-            print(f'{Fore.BLACK}{Back.LIGHTWHITE_EX}{Style.BRIGHT}{web_link} is a guest comics{Style.RESET_ALL}')
+            print(f'{Fore.BLACK}{Back.LIGHTWHITE_EX}{Style.BRIGHT}{link} is a guest comics{Style.RESET_ALL}')
 
 housepets_db['characters_db'] = list(characters_db)
 print("Saving to database...")
