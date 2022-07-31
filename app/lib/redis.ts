1821d1976e6e63d52f6addc5b944b448f9162f55
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-31 23:02:48 +0800

    Adding interfaces to redis.ts

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -1,37 +1,35 @@
-import {Client, Entity, Schema, Repository} from 'redis-om';
+import { RedisClient } from '@/utils/Interfaces';
+import { Client, Entity, Schema, Repository } from "redis-om"
 
-const client = new Client();
+const client = new Client()
 
-async function connect(){
-    if(!client.isOpen()){
-        await client.open(process.env.REDIS_URL);
-    }
+async function connect() {
+  if (!client.isOpen()) {
+    await client.open(process.env.REDIS_URL)
+  }
 }
 
-class Comic extends Entity{}
-let schema = new Schema(
-    Comic,
-    {
-        title: {type: 'string'},
-        comic_link: {type: 'string'},
-        characters: {type: 'string[]'},
-        image: {type: 'string'},
-        index: {type: 'number', sortable: true},
-    }
-)
+class Comic extends Entity {}
 
-export async function searchComics(years: string[], characters: string[]) {
-    await connect()
-    // for every year index given, search that year index that have the characters given
-    let comics = []
-    console.log(years)
-    console.log(characters)
+let schema = new Schema(Comic, {
+  title: { type: "string" },
+  comic_link: { type: "string" },
+  characters: { type: "string[]" },
+  image: { type: "string" },
+  index: { type: "number", sortable: true }
+})
 
-    years.forEach((year) => {
-        console.log(year)
-        // search the index for the year for comics that have the characters given
-        // once found, add them to the comics array
-        
-    })
-    return {"comics": comics}
+export async function searchComics<RedisClient>(years: string[], characters: string[]) {
+  await connect()
+  // for every year index given, search that year index that have the characters given
+  let comics: RedisClient[] = []
+  console.log(years)
+  console.log(characters)
+
+  years.forEach((year) => {
+    console.log(year)
+    // search the index for the year for comics that have the characters given
+    // once found, add them to the comics array
+  })
+  return { comics: comics }
 }
---
+++ app/src/utils/Interfaces.ts
@@ -0,0 +1,5 @@
+export interface RedisClient {
+  comics?: string[]
+  years: string[]
+  characters: string[]
+}
