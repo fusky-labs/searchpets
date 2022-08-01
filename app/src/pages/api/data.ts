4254f54f5d8ea2203727b18775ca8e0e38599f37
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-08-02 00:25:27 +0800

    Returned API endpoints

---
+++ app/src/pages/api/data.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { resolve } from "path"
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  const receiveData = (async () => {
+    const response = await fetch("http://localhost:5000/data")
+    const json = await response.json()
+    res.end(JSON.stringify(json))
+    resolve()
+  })()
+}
