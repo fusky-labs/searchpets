3804d2750197b3a37ac4f3d1d484ba41f1986170
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-08-15 06:08:15 +0800

    More filters and organizing

--- app/src/components/search/FilterBox.tsx
+++ app/src/components/search/FilterBox.tsx
@@ -1,7 +1,8 @@
 import { useRef, useState, useLayoutEffect } from "react"
 import styles from "@/styles/components/Filters.module.scss"
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons"
+import FilterItem from "./FilterItem"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFilter } from "@fortawesome/free-solid-svg-icons"
 
 export default function SearchContainer() {
   const generateYears = () => {
@@ -21,55 +22,52 @@ export default function SearchContainer() {
 
   useLayoutEffect(() => {
     if (!showFilter) return setFilterHeight(filterRef.current!.scrollHeight)
-    setFilterHeight(45)
+    if (window.matchMedia("(min-width: 768px)").matches) return setFilterHeight(45)
+    setFilterHeight(100)
   }, [showFilter])
 
   return (
-    <div className={styles.wrapper}>
-      <div
-        className={styles["picker-contents"]}
-        style={{
-          height: `${filterHeight}px`
-        }}
-        ref={filterRef}
-      >
-        <div
-          className={styles["detail-heading"]}
+    <div
+      className={styles.wrapper}
+      style={{
+        height: `${filterHeight}px`
+      }}
+      ref={filterRef}
+    >
+      <div className={styles.heading}>
+        <button
+          className={!showFilter ? styles.active.toString() : ""}
           onClick={() => setShowFilter(!showFilter)}
         >
-          <h2>Filters</h2>
-          <FontAwesomeIcon icon={faCaretDown} />
-        </div>
-        <div className={styles["picker-wrapper"]}>
-          <section className={styles["picker-container"]}>
-            <h2>Chronology</h2>
-            <div className={styles["filter-container"]}>
-              {yearList.map((year) => {
-                return <FilterItem yearType key={year} name={year} />
-              })}
-            </div>
-          </section>
-          <section className={styles["picker-container"]}>
-            <h2>Chapters</h2>
-            <div className={styles["filter-container"]}>
-              <FilterItem name="The Heckening" />
-              <FilterItem name="The Heckening 2" />
-              <FilterItem name="The Heckening 3" />
-            </div>
-          </section>
-        </div>
+          <FontAwesomeIcon icon={faFilter} style={{ marginRight: "0.5rem" }} />
+          Filters
+        </button>
+        <strong>Results returned text</strong>
+      </div>
+      <div className={styles["picker-wrapper"]}>
+        <section className={styles["picker-container"]}>
+          <h3>Chronology</h3>
+          <div className={styles["filter-container"]}>
+            {yearList.map((year) => {
+              return <FilterItem yearType key={year} name={year} />
+            })}
+          </div>
+        </section>
+        <section className={styles["picker-container"]}>
+          <h3>Chapters</h3>
+          <div className={styles["filter-container"]}>
+            <FilterItem name="The Heckening" />
+            <FilterItem name="The Heckening 2" />
+            <FilterItem name="The Heckening 3" />
+          </div>
+        </section>
+        <section className={styles["picker-container"]}>
+          <h3>Miscellaneous</h3>
+          <div className={styles["filter-container"]}>
+            <FilterItem name="Display guest comics" />
+          </div>
+        </section>
       </div>
     </div>
-  )
-}
-
-export function FilterItem({ name, yearType }: IFilterItemProps) {
-  let finalname = name.toString().replace(/\s/g, "-").toLowerCase()
-
-  return (
-    <span className={styles["filter-item"]}>
-      <input type="checkbox" id={yearType ? `year-${name}` : finalname} />
-      <label htmlFor={yearType ? `year-${name}` : finalname}>{name}</label>
-    </span>
   )
 }
