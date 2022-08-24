ba1632daacc0f721fca34c57340e35cacf8115c5
Restyled.io <commits@restyled.io>
2022-08-24 11:26:14 +0000

    Restyled by clang-format

--- app/next.config.js
+++ app/next.config.js
@@ -6,33 +6,22 @@
 module.exports = async (phase) => {
   const withPlugins = require("next-compose-plugins")
   const withMDX = require("@next/mdx")({
-    extension: /\.mdx?$/,
-    options: {
-      providerImportSource: "@mdx-js/react"
-    }
+    extension : /\.mdx?$/,
+    options : {providerImportSource : "@mdx-js/react"}
   })
 
   const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    compress: true,
-    experimental: {
-      nextScriptWorkers: true
-    },
-    images: {
-      domains: ["www.housepetscomic.com"],
-      formats: ["image/webp"]
-    }
+    reactStrictMode : true,
+    swcMinify : true,
+    compress : true,
+    experimental : {nextScriptWorkers : true},
+    images :
+        {domains : [ "www.housepetscomic.com" ], formats : [ "image/webp" ]}
   }
 
   const defaultConfig = {}
 
   return withPlugins(
-    [
-      withMDX({
-        pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"]
-      })
-    ],
-    nextConfig
-  )(phase, { defaultConfig })
+      [ withMDX({pageExtensions : [ "ts", "tsx", "js", "jsx", "md", "mdx" ]}) ],
+      nextConfig)(phase, {defaultConfig})
 }
