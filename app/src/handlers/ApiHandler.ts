fbe5d9c6928557d98e190beea7adf0202f8fb9ee
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-09-17 21:56:41 +0800

    Return `Promise` type from handlers

--- app/src/handlers/ApiHandler.ts
+++ app/src/handlers/ApiHandler.ts
@@ -1,27 +1,30 @@
-export function Search(years:string[], characters:string[]){
+export async function Search(
+  years: string[],
+  characters: string[]
+): Promise<SearchRes> {
   return fetch("/api/search", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        year: years.sort(),
-        characters: characters,
-      }),
-      })
-      .then((res) => res.json())
-      .then((res) => {
-        return res.comics
-      })
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      year: years.sort(),
+      characters: characters
+    })
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      return res.comics
+    })
 }
 
-export function Data(){
-  return fetch("/api/data").then((res) =>{
-      return res.json()
+export async function Data(): Promise<DataRes> {
+  return fetch("/api/data").then((res) => {
+    return res.json()
   })
 }
 
-export function Characters(){
+export async function Characters(): Promise<CharacterRes> {
   return fetch("/api/characters")
     .then((res) => res.json())
     .then((data) => {
