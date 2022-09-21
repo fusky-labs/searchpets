d7146d465c21d5aee94e3dbd2cd769aa79cbf527
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-09-21 21:29:17 +0800

    Fix absolute API endpoint problem

--- app/src/handlers/ApiHandler.ts
+++ app/src/handlers/ApiHandler.ts
@@ -1,8 +1,3 @@
-const server =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
-    : process.env.SERVER_URL
-
 type SearchHandlerTypes = SearchResponseType | SearchResponseType["comics"]
 type DataHandlerTypes = DataResponseType
 type CharacterHandlerTypes =
@@ -13,7 +8,7 @@ export async function searchHandler(
   years: string[],
   characters: string[]
 ): Promise<SearchHandlerTypes> {
-  return fetch(`${server}/api/search`, {
+  return fetch("/api/search", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -30,11 +25,16 @@ export async function searchHandler(
 }
 
 export async function dataHandler(): Promise<DataHandlerTypes> {
+  const server =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : process.env.SERVER_URL
+
   return await fetch(`${server}/api/data`).then((res) => res.json())
 }
 
 export async function characterHandler(): Promise<CharacterHandlerTypes> {
-  return fetch(`${server}/api/characters`)
+  return fetch("/api/characters")
     .then((res) => res.json())
     .then((data: CharactersArrayType) => {
       return data.characters_db
