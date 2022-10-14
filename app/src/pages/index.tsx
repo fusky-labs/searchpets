652652187fedcd87d40961a9bb2604df152b7b77
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-10-15 01:23:46 +0800

    Revert "Use ISR and added Dockerfile for testing"
    This reverts commit 3127add430f343adeb7e8cd72be27d1262323204.

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,25 +1,19 @@
 import Container from "@/components/Base/Container"
-import Link from "next/link"
-import { GetStaticProps } from "next"
+import { GetServerSideProps } from "next"
 import { dataHandler } from "../handlers/ApiHandler"
 
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await dataHandler().then((res) => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = dataHandler().then((res) => {
     return res
   })
-
-  return {
-    props: { data },
-    revalidate: 120
-  }
+  return { props: data }
 }
 
 export default function Home({ comicCount, charCount }: DataResponseType) {
   return (
     <Container>
       <div className="text-center flex flex-col items-center w-full">
-        <Link href="/characters">{charCount}</Link> characters and {comicCount}
-        comics
+        {charCount} characters and {comicCount} comics
         <div>searchbox here</div>
       </div>
     </Container>
